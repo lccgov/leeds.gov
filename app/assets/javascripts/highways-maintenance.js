542ceeb9e8e5af7bb0258052cb4ceefbc489a6b4
Nigel Smyth <Nigelsmyth@hotmail.com>
2017-10-25 16:07:22 +0100

    Made changes to Street Register API

--- app/assets/javascripts/highways-maintenance.js
+++ app/assets/javascripts/highways-maintenance.js
@@ -10,10 +10,21 @@
         this.start = function (element) {
 
             $(document).ready(function () {
+                
+                $("#tblHighways").hide();
+
+                $('#full-name-f1').keypress(function (event) {
+                    if (event.keyCode == 13) {
+                        $('#showStreet').click();
+                        return false;
+                    }
+                });
 
                 $("#showStreet").click(function (e) {
 
+                    $("#lblMessage").html('Please wait...');
                     $("#tblResults").html('');
+                    $("#tblHighways").hide();
 
                     var ward = 'ALL';
 
@@ -23,29 +34,51 @@
                     }
 
                     $.getJSON('https://api.highwaysmaintenance.leeds.gov.uk/api/getSearchdata2014_Result/' + ward +'/' + street + '/Ward', function (data) {
-                        $.each(data, function (key, value) {
-                            var markup1 = "<tr><td>" + value.Promoter + "</td><td>" + value.Road + "</td><td>" + value.From___To + "</td><td>" + value.Nature_of_Works + "</td><td>" + value.Start_Date + "</td><td>" + value.Duration__weeks_ + "</td><td>" + value.Description + "</td></tr>";
-                            $("#tblResults").append(markup1);
-                        });
+
+                        if (data.length == 0) {
+                            $("#lblMessage").html('No results returned');
+                        }
+                        else
+                        {
+                            $("#lblMessage").html(data.length + ' result(s) found');
+                            $("#tblHighways").show();
+                            $.each(data, function (key, value) {
+                                var markup1 = "<tr><td>" + value.Promoter + "</td><td>" + value.Road + "</td><td>" + value.From___To + "</td><td>" + value.Nature_of_Works + "</td><td>" + value.Start_Date + "</td><td>" + value.Duration__weeks_ + "</td><td>" + value.Description + "</td></tr>";
+                                $("#tblResults").append(markup1);
+                            });
+                        }
                     });
+                    $("#maintenance-programme_key").show();
                 });
 
                 $("#select-box").change(function () {
                     var street = 'ALL';
                     var ward = this.value;
-                    if (ward == 'All wards') 
-                    {
-                        ward = 'ALL';
-                    }
+                    if (ward != '') {
+                        $("#lblMessage").html('Please wait...');
+                        if (ward == 'All wards') 
+                        {
+                            ward = 'ALL';
+                        }
 
-                    $("#tblResults").html('');
+                        $("#tblResults").html('');
 
-                    $.getJSON('https://api.highwaysmaintenance.leeds.gov.uk/api/getSearchdata2014_Result/' + ward + '/' + street + '/Ward', function (data) {
-                        $.each(data, function (key, value) {
-                            var markup1 = "<tr><td>" + value.Promoter + "</td><td>" + value.Road + "</td><td>" + value.From___To + "</td><td>" + value.Nature_of_Works + "</td><td>" + value.Start_Date + "</td><td>" + value.Duration__weeks_ + "</td><td>" + value.Description + "</td></tr>";
-                            $("#tblResults").append(markup1);
+                        $.getJSON('https://api.highwaysmaintenance.leeds.gov.uk/api/getSearchdata2014_Result/' + ward + '/' + street + '/Ward', function (data) {
+                            if (data.length == 0) {
+                                $("#lblMessage").html('No results returned');
+                            }
+                            else
+                            {
+                                $("#lblMessage").html(data.length + ' result(s) found');
+                                $("#tblHighways").show();
+                                $.each(data, function (key, value) {
+                                    var markup1 = "<tr><td>" + value.Promoter + "</td><td>" + value.Road + "</td><td>" + value.From___To + "</td><td>" + value.Nature_of_Works + "</td><td>" + value.Start_Date + "</td><td>" + value.Duration__weeks_ + "</td><td>" + value.Description + "</td></tr>";
+                                    $("#tblResults").append(markup1);
+                                });
+                            }
                         });
-                    });
+                    }
+                    $("#maintenance-programme_key").show();
                 });
 
                 $.getJSON('https://api.highwaysmaintenance.leeds.gov.uk/api/getTreatmentTypes_Result', function (data) {
@@ -53,6 +86,7 @@
                         var markup2 = "<tr><td>" + value.Code + "</td><td>" + value.Treatment + "</td><td>" + value.Description + "</td></tr>";
                         $("#tblTreatmentTypes").append(markup2);
                     });
+                    $("#maintenance-programme_key").hide();
                 });
             });
         };
--- app/assets/javascripts/street-register.js
+++ app/assets/javascripts/street-register.js
@@ -10,6 +10,9 @@
         this.start = function (element) {
 
             LCC.Modules.StreetRegister.getStreetData = function (roadId) {
+                $("#street-register_details").show();
+                $("#street-register_results").hide();
+
                 $.getJSON('https://api.streetregister.leeds.gov.uk/api/getAddress_Result/' + roadId, function (data) {
                     LCC.Modules.StreetRegister.clearData();
                     $.each(data, function (key, value) {
@@ -23,24 +26,45 @@
                         $("#divReference").append("<p>" + value.NSG + "</p>");
 
                         if (data[0].FoothpathFrom != null) {
+                            $("#footpaths").show();
                             for (var i = 0; i < data[0].FoothpathFrom.length; i++) {
                                 $("#tblFootpaths").append("<tr><td>" +  data[0].FoothpathFrom[i] + "</td><td>" + data[0].FoothpathTo[i] + "</td><td>" + data[0].FoothpathStatus[i] + "</td></tr>");
                             }
                         }
+                        else
+                        {
+                            $("#footpaths").hide();
+                        }
 
                         if (data[0].UnnamedStreetFrom != null) {
+                            $("#un-named-streets").show();
                             for (var i = 0; i < data[0].UnnamedStreetFrom.length; i++) {
                                 $("#tblUnnamedStreets").append("<tr><td>" + data[0].UnnamedStreetFrom[i] + "</td><td>" + data[0].UnnamedStreetTo[i] + "</td><td>" + data[0].UnnamedStreetStatus[i] + "</td></tr>");
                             }
                         }
+                        else
+                        {
+                            $("#un-named-streets").hide();
+                        }
 
                         if (data[0].SplitStreetsFrom != null) {
+                            $("#split-streets").show();
                             for (var i = 0; i < data[0].SplitStreetsFrom.length; i++) {
                                 $("#tblSplitStreets").append("<tr><td>" + data[0].SplitStreetsFrom[i] + "</td><td>" + data[0].SplitStreetsTo[i] + "</td><td>" + data[0].SplitStreetsStatus[i] + "</td></tr>");
                             }
                         }
+                        else
+                        {
+                            $("#split-streets").hide();
+                        }
                     });
                 });
+            };
+
+            LCC.Modules.StreetRegister.ShowResults =  function() {
+                LCC.Modules.StreetRegister.clearData();
+                $("#street-register_details").hide();
+                $("#street-register_results").show();
             };
         
             LCC.Modules.StreetRegister.clearData =  function() {
@@ -57,13 +81,36 @@
             };
 
             $(document).ready(function () {
+                $("#tblStreetData").hide();
+                $("#street-register_details").hide();
+
+                $('#streetName').keypress(function (event) {
+                    if (event.keyCode == 13) {
+                        $('#streetLookup').click();
+                        return false;
+                    }
+                });
+
                 $("#streetLookup").click(function (e) {
+                    LCC.Modules.StreetRegister.ShowResults();
                     $("#tblBodyDetails").html('');
+                    $("#lblMessage").html("Please wait...");
+                    $("#tblStreetData").hide();
+                    $("#street-register_details").hide();
+
                     $.getJSON('https://api.streetregister.leeds.gov.uk/api/getAddressSearch_Result/' + $('#streetName').val(), function (data) {
-                        $.each(data, function (key, value) {
-                            var address = "<tr><td>" + value.RoadName + "</td><td>" + value.Area + "</td><td>" + value.Status + "</td><td>" + "<td> <a href='#' onClick='LCC.Modules.StreetRegister.getStreetData(" + value.RoadID + ")'>Details</a> </td></tr>";
-                            $("#tblBodyDetails").append(address);
-                        });
+                        if (data.length == 0) {
+                            $("#lblMessage").html('No results returned');
+                        }
+                        else
+                        {
+                            $("#lblMessage").html(data.length + " result(s) found ");
+                            $("#tblStreetData").show();
+                            $.each(data, function (key, value) {
+                                var address = "<tr><td>" + value.RoadName + "</td><td>" + value.Area + "</td><td>" + value.Status + "</td><td>" + "<a href='#/' onClick='LCC.Modules.StreetRegister.getStreetData(" + value.RoadID + ")'>Details</a> </td></tr>";
+                                $("#tblBodyDetails").append(address);
+                            });
+                        }
                     });
                 });
             });
