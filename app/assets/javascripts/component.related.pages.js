a8bd4ee054bf67a4bb93ba315a70617ecae0c018
leeds\adm-tco04004 <matt.williams@leeds.gov.uk>
2017-11-27 10:48:17 +0000

    make related pages use search

--- app/assets/javascripts/component.related.pages.js
+++ app/assets/javascripts/component.related.pages.js
@@ -28,8 +28,15 @@
                     self.pages = params.relatedPages;
                     self.pageTitle = params.pageTitle || "Related Pages";
                 },
-                template: '<h3 data-bind="text: pageTitle"></h3><ul class="nav" data-bind="foreach: pages"><li data-bind="text: name"><span data-bind="text: term.get_name()"></span></li></ul>'
+                template: '<h3 data-bind="text: pageTitle"></h3><ul class="nav" data-bind="foreach: pages"><li><a data-bind="text: name, attr: { href: url, title: name }" class="item"></a></li></ul>'
             });
+
+            var RelatedPage = function(data) {
+                var self = this;
+                self.name = ko.observable(data.Cells.results[5].Value); 
+                // or plain "self.name = data.name;" if you don't need 2way binding
+                self.url = data.Cells.results[2].Value;
+            }
 
             var vm = function () {
                 var self = this;
@@ -46,28 +53,18 @@
                                     jQuery.when(LCC.Services.SharePoint.GetCurrentPageSPItem())
                                     .then(function(listItem) {
                                         var pageTerms = LCC.Services.SharePoint.GetPageRelatedTerms(listItem);
-                                        //console.log(pageTerms);
-                                        //self.pages(pageTerms);
                                         
                                         if(typeof(pageTerms) !== 'undefined') {
 
-                                            for (var i = 0; i < pageTerms.length; i++) {
-                                            //pageTerms.map(function(currentValue) {
-                                                var currentValue = pageTerms[i];
-                                                jQuery.when(LCC.Services.SharePoint.GetTerm(currentValue))
-                                                .then(function(termData) {
-                                                    //console.log(term);
-                                                      jQuery.when(LCC.Services.SharePoint.GetTermSetOfTerm(termData))
-                                                      .then(function(fullData) {
-                                                         console.log(fullData);
-                                                         self.pages.push(fullData);
-                                                      });
-                                                });
-                                                
-                                            }
-                                            //});
-
-                                            
+                                            jQuery.when(LCC.Services.SharePoint.GetRelatedPages(pageTerms))
+                                            .then(function (relatedData) {
+                                                self.pages(relatedData.map(function(item) { 
+                                                    return new RelatedPage(item); 
+                                                }));
+                                            },
+                                            function(error) {
+                                                console.log(error);
+                                            });
                                         }
                                     },
                                     function(error) {
@@ -78,30 +75,6 @@
                         });
                 
                 });
-
-                //SP.SOD.registerSod('sp.taxonomy.js', SP.Utilities.Utility.getLayoutsPageUrl('sp.taxonomy.js'));   
-            //     SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function() {
-            //     SP.SOD.executeFunc('sp.taxonomy.js', 'SP.Taxonomy.TaxonomySession', function () {   
-            //     jQuery.when(LCC.Services.SharePoint.GetCurrentPageSPItem())
-            //     .then(function(listItem) {
-            //         var pageTerms = LCC.Services.SharePoint.GetPageRelatedTerms(listItem);
-            //         if(typeof(pageTerms) !== 'undefined') {
-            //             pageTerms.map(function(currentValue) {
-            //                 jQuery.when(LCC.Services.SharePoint.GetTerm(currentValue.termId))
-            //                 .then(function(data) {
-            //                     console.log(data);
-            //                 });
-            //             });
-
-            //             console.log(pageTerms);
-            //             self.pages(pageTerms);
-            //         }
-            //     },
-            //     function(error) {
-            //         console.log(error);
-            //     });
-            // });
-            // }, "sp.js");
             }
 
             ko.applyBindings(new vm(), element[0]);
